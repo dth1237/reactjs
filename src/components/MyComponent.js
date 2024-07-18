5a9d5c544e3c3894579090158cb9745eec39e8f7
dth1237 <121176268+dth1237@users.noreply.github.com>
2024-07-19 00:00:02 +0700

    component`

---
+++ src/components/MyComponent.js
@@ -0,0 +1,15 @@
+//Class compoents
+//funcion components
+
+import React from "react";
+class MyComponent extends React.Component{
+    //JSX
+    render(){
+        return (
+            <div>my first components</div>
+        );
+
+    }
+}
+
+export default MyComponent;
