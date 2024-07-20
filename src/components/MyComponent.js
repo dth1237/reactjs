0f9a432a6d58ec1ae54c0402281aa872fecedbe6
dung <nguyenngocdung8g@gmail.com>
2024-07-21 00:46:49 +0700

    Dom_event

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -2,17 +2,28 @@
 //funcion components
 
 import React from "react";
-class MyComponent extends React.Component{
+class MyComponent extends React.Component {
     state = {
         name: `d`,
         age: `20`
 
     };
+    handelClick(event) {
+        console.log("Click me my button ")
+        console.log("My name is" , this.state.name);
+
+    }
+    HandleonMouseover(event) {
+        console.log(event.pageX)
+
+    }
     //JSX
-    render(){
+    render() {
         return (
-            <div> 
-                 My name is the {this.state.name} 
+            <div>
+                My name is the {this.state.name}
+                <button onClick={this.handelClick}>click me</button>
+                <button onMouseOver={this.HandleonMouseover}>over</button>
             </div>
         );
 
