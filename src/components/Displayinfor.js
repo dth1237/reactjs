5bf645a19230824378fa80545932d808d066f400
dung <nguyenngocdung8g@gmail.com>
2024-07-24 23:15:42 +0700

    conditional output

--- src/components/Displayinfor.js
+++ src/components/Displayinfor.js
@@ -3,35 +3,54 @@ import React from "react";
 class Displayinfor extends React.Component
 {
 //prop
+    state = {
+        isshowisuser:true
+
+    }
+    handlshowhide =()=>
+    {
+        this.setState(
+        {
+            isshowisuser:!this.state.isshowisuser
+        })
+    }
     render(){
        const{listUser} = this.props;
-       console.log(listUser);
         return (
             <div>
+                <div>
+                    <span onClick={()=>{this.handlshowhide()}}> 
+                       
+                            {this.state.isshowisuser == true ? "Hide list user" : "show user"}</span>
+                </div>
+
+          
+
+           
+           {this.state.isshowisuser && <div>
                 {listUser.map ((user) => {
+                    // if (+user.age < 18)
+                    
                     return (
-                        <div key={user.id}>
+                        <div key={user.id} className = {+user.age > 18 ? 'green' : 'red'}>
                            <div> My name is {user.name}</div>
                             <div> My age is {user.age}</div>
 
                     </div>
                     )
                     
+               
 
                 })}
 
 
 
 
-                {/* <div>
-                My name is {this.props.name}
-                </div>
-                <div>
-                My age   {this.props.age}
-                 </div> */}
+             
 
 
 
+            </div>}
             </div>
         )
     }
