ccd5a9887b59447d946a562cee7286a5f0f15120
dung <nguyenngocdung8g@gmail.com>
2024-08-03 22:42:56 +0700

    design-homepage

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -8,11 +8,12 @@ const Header = () => {
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
             <Container>
+                <NavLink to='/' className='navbar-brand '>Quizz</NavLink>
+
 
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <NavLink to='/' className='navbar-brand '>Quizz</NavLink>
 
                         <NavLink to='/' className='nav-link'>Home</NavLink>
                         <NavLink to='/users' className='nav-link'>User</NavLink>
@@ -22,12 +23,15 @@ const Header = () => {
 
                     </Nav>
                     <Nav>
-                        <NavDropdown title="Setting" id="basic-nav-dropdown">
+                        <button className='btn-login'>Login</button>
+                        <button className='btn-signup'>Sign up</button>
+
+                        {/* <NavDropdown title="Setting" id="basic-nav-dropdown">
                             <NavDropdown.Item>Log in</NavDropdown.Item>
                             <NavDropdown.Item > Log out </NavDropdown.Item>
                             <NavDropdown.Item>Profile</NavDropdown.Item>
 
-                        </NavDropdown>
+                        </NavDropdown> */}
                     </Nav>
                 </Navbar.Collapse>
             </Container>
